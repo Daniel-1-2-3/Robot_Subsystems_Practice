b73de6c886dfc8bac83505e0d530a711bd55e535
Daniel-1-2-3 <danielzhu501@gmail.com>
2023-11-03 21:43:46 -0400

    Initial commit

---
+++ src/main/java/frc/robot/Subsystems/Drive.java
@@ -0,0 +1,15 @@
+package frc.robot.Subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drive extends SubsystemBase{
+    public static Drive instance;
+
+    public static Drive getInstance() {
+        return instance == null ? instance = new Drive() : instance;
+    }
+
+    @Override
+    public void periodic() {
+        //...
+    }
+}
